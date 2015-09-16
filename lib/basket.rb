f6280ff5ed010721bd003baababb53b0e876de8d
Matt Paul <mattpaul001@icloud.com>
2015-09-16 20:19:48 +0100

    Items can be added and removed from the basket

--- lib/basket.rb
+++ lib/basket.rb
@@ -1,17 +1,40 @@
 class Basket
 
-    attr_reader :basket_items
+    attr_reader :basket_items  # => nil
 
     def initialize
-      @basket_items = []
+      @basket_items = []  # => []
     end
 
-    def add_to_basket(item)
-      @basket_items << item
+    def add_to_basket(item,quantity)
+      quantity.times {@basket_items.push(item)}  # => 1, 1, 1
     end
 
     def empty_basket
-      @basket_items.clear
+      @basket_items.clear  # => []
     end
 
+    def view_basket
+      @basket_items  # => [:paneer]
+    end
+
+    def basket_price
+    end
+
+    def remove_basket_item(item)
+      @basket_items.delete(item)  # => :naan
+    end
+
+
 end
+
+
+
+# basket = Basket.new               # => #<Basket:0x007ffa7c062f10 @basket_items=[]>
+# basket.add_to_basket(:naan,1)     # => 1
+# basket.empty_basket               # => []
+# basket.add_to_basket(:naan,1)     # => 1
+# basket.add_to_basket(:paneer,1)   # => 1
+# basket.remove_basket_item(:naan)  # => :naan
+# basket.view_basket                # => [:paneer]
+# basket.basket_price               # => nil
--- spec/basket_spec.rb
+++ spec/basket_spec.rb
@@ -1,23 +1,39 @@
-require 'basket'
+require 'basket'  # ~> LoadError: cannot load such file -- basket
 
 describe Basket do
 
     it 'can add an item into a basket' do
       naan = double(:fakenaan)
-      subject.add_to_basket(naan)
+      subject.add_to_basket(naan,1)
       expect(subject.basket_items).to eq([naan])
     end
 
     it 'can clear all items from basket' do
       naan = double(:fakenaan)
-      subject.add_to_basket(naan)
+      subject.add_to_basket(naan,2)
       subject.empty_basket
       expect(subject.basket_items.length).to eq(0)
     end
 
-    # it 'can view all items in the basket' do
+    it 'can view all items in the basket' do
+      naan = double(:fakenaan)
+      subject.add_to_basket(naan,1)
+      expect(subject.view_basket).to eq([naan])
+    end
+
+    # it 'can add up all the items in the basket' do
     #   naan = double(:fakenaan)
-    #   subject
+    #   paneer = double(:paneer)
+    #   subject.add_to_basket(naan,1)
+    #   subject.add_to_basket(paneer,1)
+    #   expect(subject.basket_price).to eq(300)
     # end
+
+    it 'can remove an item from the basket' do
+      naan = double(:fakenaan)
+      subject.add_to_basket(naan,1)
+      subject.remove_basket_item(naan)
+      expect(subject.basket_items.length).to eq(0)
+    end
 
 end
